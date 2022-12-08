d017c33af9b8587721de8e901ec9b0f5589d041d
Drew Taylor <116771412+dtaylor338@users.noreply.github.com>
2022-12-07 19:03:24 -0600

    The dictionaries containing all the vehicles

---
+++ FP-Dictionaries/FP_Chevrolet.py
@@ -0,0 +1,5 @@
+dict = {1:{'A': 'chevrolet', 'B': 'silverado', 'C':'2018', 'D':'35,000'}, 
+        2:{'A': 'chevrolet', 'B': 'malibu', 'C':'2019', 'D':'14,000'},
+        3:{'A': 'chevrolet', 'B': 'sonic', 'C':'2014', 'D':'8,000'},
+        4:{'A': 'chevrolet', 'B': 'colorado', 'C':'2020', 'D':'26,000'}}
+        
