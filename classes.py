6e8004ce7088552daed1f10945d75d19328d3da6
cwells134 <103548078+cwells134@users.noreply.github.com>
2022-12-15 01:18:13 -0600

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,146 +1,100 @@
-import inventory_dictionaries
+from inventory_dictionaries import *
 
 
+class new_search:
+    def __init__(self):
+        self.result = []
 
-
-class new_search():
-    def __init__(self, model_var, price_var):
-        self.model_var = model_var
-        self.price_var = price_var
-
-    def model_search(self):
+    def model_search(self, model_var):
         for x in new_inventory:
             for n in new_inventory[x]:
-                if n == self.model_var:
-                    result = new_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if n == model_var:
+                    self.result.append(new_inventory[x])
+        return self.result
 
-    def price_search(self):
+
+    def price_search(self, price_var):
         for x in new_inventory:
             for n in new_inventory[x]:
-                if n <= self.price_var:
-                    result = new_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if type(n) == int:
+                    if n <= price_var:
+                        self.result.append(new_inventory[x])
+        return self.result
 
 
-class used_search():
-    def __init__(self, make_var, model_var, year_var, price_var):
-        self.make_var = make_var
-        self.model_var = model_var
-        self.year_var = year_var
-        self.price_var = price_var
 
-    def make_search(self):
-        for x in used_inventory:
-            for n in used_inventory:
-                if n == self.model_var:
-                    result = used_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+class used_search:
+    def __init__(self):
+        self.result = []
 
-    def model_search(self):
+    def make_search(self, make_var):
         for x in used_inventory:
             for n in used_inventory[x]:
-                if n == self.model_var:
-                    result = used_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if n == make_var:
+                    self.result.append(used_inventory[x])
+        return self.result
 
-    def year_search(self):
+
+    def model_search(self, model_var):
         for x in used_inventory:
             for n in used_inventory[x]:
-                if n == self.year_var:
-                    result = used_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if n == model_var:
+                    self.result.append(used_inventory[x])
+        return self.result
 
-    def price_search(self):
+
+    def year_search(self, year_var):
         for x in used_inventory:
             for n in used_inventory[x]:
-                if n <= self.price_var:
-                    result = used_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if type(n) == int:
+                    if n == year_var:
+                        self.result.append(used_inventory[x])
+        return self.result
 
-class all_search():
-    def __init__(self, make_var, model_var, year_var, price_var, used_var, new_var):
-        self.make_var = make_var
-        self.model_var = model_var
-        self.year_var = year_var
-        self.price_var = price_var
-        self.used_var = used_var
-        self.new_var = new_var
+    def price_search(self, price_var):
+        for x in used_inventory:
+            for n in used_inventory[x]:
+                if type(n) == int:
+                    if n <= price_var:
+                        self.result.append(used_inventory[x])
+        return self.result
 
-    def make_search(self):
+
+class all_search:
+    def __init__(self):
+        self.result = []
+
+    def make_search(self, make_var):
         for x in all_inventory:
             for n in all_inventory:
-                if n == self.model_var:
-                    result = all_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if n == make_var:
+                    self.result.append(all_inventory[x])
+        return self.result
 
-    def model_search(self):
+
+    def model_search(self, make_var):
         for x in all_inventory:
             for n in all_inventory[x]:
-                if n == self.model_var:
-                    result = all_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if n == make_var:
+                    self.result.append(all_inventory[x])
+        return self.result
 
-    def year_search(self):
+
+    def year_search(self, year_var):
         for x in all_inventory:
             for n in all_inventory[x]:
-                if n == self.year_var:
-                    result = all_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if type(n) == int:
+                    if n == year_var:
+                        self.result.append(all_inventory[x])
+        return self.result
 
-    def price_search(self):
+
+    def price_search(self, price_var):
         for x in all_inventory:
             for n in all_inventory[x]:
-                if n <= self.price_var:
-                    result = all_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
+                if type(n) == int:
+                    if n <= price_var:
+                        self.result = all_inventory[x]
+        return self.result
 
-    def usedInv_search(self):
-        for x in all_inventory:
-            for n in all_inventory[x]:
-                if n == self.used_var:
-                    result =all_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
 
-def newInv_search(self):
-        for x in all_inventory:
-            for n in all_inventory[x]:
-                if n == self.new_var:
-                    result =all_inventory[x]
-                    return result
-                else:
-                    result = 'No vehicles found'
-                    return result
---
+++ inventory_search.py
@@ -0,0 +1,142 @@
+import tkinter as tk
+from tkinter.ttk import Treeview
+from tkinter import *
+from classes import *
+
+
+# Main window options
+window = tk.Tk()
+window.title('Kenny Kent Chevrolet Inventory Search')
+window.geometry('300x200')
+window.columnconfigure(0, weight=1)
+window.columnconfigure(4, weight=1)
+
+
+# Variables
+used_var = tk.IntVar()
+new_var = tk.IntVar()
+make_var = tk.StringVar()
+model_var = tk.StringVar()
+year_var = tk.IntVar()
+price_var = tk.IntVar()
+result_set = []
+
+
+# New/Used Checkboxes
+used_box = tk.Checkbutton(window, text='Used', variable=used_var, onvalue=1, offvalue=0)
+new_box = tk.Checkbutton(window, text='New', variable=new_var, onvalue=1, offvalue=0)
+used_box.grid(row=1, column=2)
+new_box.grid(row=1, column=3)
+
+# Make entry box
+make_label = tk.Label(window, text='Make: ')
+make_entry = tk.Entry(window, textvariable=make_var)
+make_label.grid(row=2, column=2)
+make_entry.grid(row=2, column=3)
+
+# Model entry box
+model_label = tk.Label(window, text='Model: ')
+model_entry = tk.Entry(window, textvariable=model_var)
+model_label.grid(row=3, column=2)
+model_entry.grid(row=3, column=3)
+
+# Year entry box
+year_label = tk.Label(window, text='Year: ')
+year_entry = tk.Entry(window, textvariable=year_var)
+year_label.grid(row=4, column=2)
+year_entry.grid(row=4, column=3)
+
+# Price Entry
+price_label = tk.Label(window, text='Price')
+price_entry = tk.Entry(window, textvariable=price_var)
+price_label.grid(row=5, column=2)
+price_entry.grid(row=5, column=3)
+
+
+# Search Button Click
+def search():
+    if used_var.get() == 1:
+        used_inventory_search()
+    elif new_var.get() == 1:
+        if make_var.get().lower() != 'chevrolet' or make_var.get().lower() != 'chevy':
+            error()
+        else:
+            new_search()
+    else:
+        if not make_var.get() and not model_var.get() and not year_var.get():
+            error()
+        else:
+            all_search()
+
+
+# Search button
+search_button = tk.Button(window, text='Search', command=search)
+search_button.grid(row=7, column=3)
+
+
+# Error Window
+def error():
+    error_window = tk.Tk()
+    error_window.title('Error')
+    error_label = tk.Label(error_window, text='Invalid Input')
+    error_label.pack()
+
+
+# Used Search Function
+def used_inventory_search():
+    us = used_search()
+    make = make_var.get()
+    model = model_var.get()
+    year = int(year_var.get())
+    price = int(price_var.get())
+    if make:
+        make_result = us.make_search(make)
+        for n in make_result:
+            if n not in result_set:
+                result_set.append(n)
+    if model:
+        model_result = us.model_search(model)
+        for n in model_result:
+            if n not in result_set:
+                result_set.append(n)
+    if year:
+        year_result = us.year_search(year)
+        for n in year_result:
+            if n not in result_set:
+                result_set.append(n)
+    if price:
+        price_result = us.price_search(price)
+        for n in price_result:
+            if n not in result_set:
+                result_set.append(n)
+    result_window()
+
+
+# Search Result Window
+def result_window():
+    result_win = tk.Tk()
+    result_win.title('Results')
+
+    tree = Treeview(result_win, columns=('Make', 'Model', 'Year', 'Price'), show='headings')
+    tree["columns"] = ('Make', 'Model', 'Year', 'Price')
+
+    tree.column('#1', anchor=CENTER)
+    tree.heading('#1', text='Make')
+    tree.column('#2', anchor=CENTER)
+    tree.heading('#2', text='Model')
+    tree.column('#3', anchor=CENTER)
+    tree.heading('#3', text='Year')
+    tree.column('#4', anchor=CENTER)
+    tree.heading('#4', text='Price')
+
+    tree.pack()
+    for i, n in enumerate(result_set):
+        col1 = n[0]
+        col2 = n[1]
+        col3 = n[2]
+        col4 = n[3]
+
+        tree.insert('', 'end', text=f'{i}', values=(col1, col2, col3, col4))
+
+
+window.mainloop()
