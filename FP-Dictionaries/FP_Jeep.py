446e6f21514417fc2534932577191e123f039dd8
cwells134 <103548078+cwells134@users.noreply.github.com>
2022-12-14 21:49:00 -0600

    Delete FP-Dictionaries directory

--- FP-Dictionaries/FP_Jeep.py
+++
@@ -1,4 +0,0 @@
-dict = {1:{'A': 'jeep', 'B': 'wrangler', 'C':'2018', 'D':'35,000'}, 
-        2:{'A': 'jeep', 'B': 'grand cherokee', 'C':'2019', 'D':'24,000'},
-        3:{'A': 'jeep', 'B': 'cherokee', 'C':'2014', 'D':'8,000'},
-        4:{'A': 'jeep', 'B': 'gladiator', 'C':'2020', 'D':'26,000'}}
--- FP-Dictionaries/FP_New:Chevrolet.py
+++
@@ -1,5 +0,0 @@
-dict = {1:{'A': 'chevrolet', 'B': 'silverado', 'C':'2022', 'D':'35,000'}, 
-        2:{'A': 'chevrolet', 'B': 'malibu', 'C':'2022', 'D':'24,000'},
-        3:{'A': 'chevrolet', 'B': 'sonic', 'C':'2022', 'D':'18,000'},
-        4:{'A': 'chevrolet', 'B': 'colorado', 'C':'2022', 'D':'29,000'}}
-        
